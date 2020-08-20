9c6bed83980c94f6e9bc4e5ce0172563a777c667
Aaron Bruce <aaronbruce555@gmail.com>
2020-08-20 13:37:38 -0400

    Standardized the timezone to eastern between unix and utc dates

--- src/components/protectedRoutes/Graph.js
+++ src/components/protectedRoutes/Graph.js
@@ -16,36 +16,30 @@ export const Graph = (props) => {
   const { getUser, userId, authAxios } = useContext(PocketBrokerContext);
   const [cryptoName, setCryptoName] = useState(props.cryptoName);
   const [timeFrame, setTimeFrame] = useState(props.timeFrame);
+  const [startTime, setStartTime] = useState(undefined);
+  const [endTime, setEndTime] = useState(undefined);
+  const [minValue, setMinValue] = useState(null);
+  const [maxValue, setMaxValue] = useState(null);
   const [currentData, setCurrentData] = useState([]);
 
   // Unix Epoch TimeFrames in seconds
+  const one_hour_unix = 3600;
   const one_day_unix = 86400;
   const one_week_unix = 604800;
   const one_month_unix = 2629743;
   const one_year_unix = 31556926;
   // const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const monthsOfTheYear = ["Jan", "Feb", "Mar", "Apl", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const monthsOfTheYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-  // Returns date as UTC
+  // Returns date from unix in seconds
   const unixToDate = (unix) => {
-    // convert to milliseconds
-    let date = new Date(unix);
-    let month = monthsOfTheYear[date.getMonth()];
-    let year = date.getFullYear();
-    let dateNum = date.getDate();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let newDate = `${year} ${month} ${dateNum} ${hours}:${minutes}:${seconds}`;
-    // toISOString returns date as UTC
-    // console.log(newDate)
-    newDate = new Date(newDate).toISOString()
-    newDate.split('-').join(' ')
-    return new Date(newDate);
-    // return Date.UTC(year, month, dateNum, hours, minutes, seconds) / 1000;
+    // converts to eastern standard time for
+    unix += (one_hour_unix * 4);
+    // converts to milliseconds
+    return new Date(unix*1000);
   }
 
-  // returns unix as UTC
+  // returns unix as seconds
   const dateToUnix = (date) => {
     let year = date.getFullYear();
     let month = date.getMonth();
@@ -53,45 +47,47 @@ export const Graph = (props) => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    // convert to seconds
-    return Date.UTC(year, month, day, hours, minutes, seconds);
+    // converts unix to seconds
+    return Date.UTC(year, month, day, hours, minutes, seconds) / 1000;
   }
 
-  const getDayStart = () => {
+  // returns today's date with the hours, minutes, seconds, and millisec zeroed out
+  const getDayStartDateTime = () => {
     let today = new Date();
     today.setHours(0)
     today.setMinutes(0)
     today.setSeconds(0)
     today.setMilliseconds(0);
-    // setStartDatetime(today); // sets the start time for the beginning of today.
+    setStartTime(today); // sets the start time for the beginning of today.
     return today;
   }
 
-  // returns in local time, needs to convert to UTC
-  const getTimeAgo = () => {
+
+  // returns in local time
+  const getUnixTimeAgo = () => {
     setTimeFrame(timeFrame.toLowerCase().trim());
-    // setTimeFrame(timeFrame);
-    let now = new Date();
-    now = dateToUnix(now);
+    let end = new Date();
+    end = dateToUnix(end);
     let start;
     switch (timeFrame) {
       case "day":
-        start = dateToUnix(getDayStart());
+        start = dateToUnix(getDayStartDateTime());
         break;
       case "week":
-        start = now - one_week_unix;
+        start = end - one_week_unix;
         break;
       case "month":
-        start = now - one_month_unix;
+        start = end - one_month_unix;
         break;
       case "year":
-        start = now - one_year_unix;
+        start = end - one_year_unix;
         break;
       default:
         return "Invalid timeframe"
     }
     let modifiedStart = unixToDate(start)
-    // setStartDatetime(modifiedStart);  // should convert unix to UTC date and set as state
+    setEndTime(modifiedStart)
+    // convert back to unix
     let year = modifiedStart.getFullYear();
     let month = modifiedStart.getMonth();
     let day = modifiedStart.getDate();
@@ -104,53 +100,43 @@ export const Graph = (props) => {
   useEffect(() => {
     (async function () {
       // setCryptoName(cryptoName); // set the state to be the crypto name
-      let start_time = getTimeAgo(timeFrame);
-      let end_time = Math.floor(Date.now() / 1000);
+      let start_time = getUnixTimeAgo(); // 1597867200
+      let end_time = dateToUnix(new Date()); // 1597928927
       const api = `${coinGecko}/coins/${cryptoName}/market_chart/range?vs_currency=usd&from=${start_time}&to=${end_time}/`;
       let data = await fetch(api);
       let json = await data.json();
-      // console.log("we're in the useEffect", json);
       setCurrentData(json.prices);
-      console.log("we're in the useEffect", currentData);
     })();
   }, []);
 
-  // sample data for demonstrative purposes (Temporary)
-  // const memoizeGenerateRandomData = useCallback(
-  //   (noOfDps) => {
-  //     var xVal = 1, yVal = 100;
-  //     var dps = [];
-  //     for (var i = 0; i < noOfDps; i++) {
-  //       yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
-  //       dps.push({ x: xVal, y: yVal });
-  //       xVal++;
-  //     }
-  //     console.log(dps)
-  //     return dps;
-  //   },
-  //   [],
-  // );
+  // get the percent change between 2 numbers
+  const getPercentChange = (val1, val2) => {
+    if(val1 === null || val2 === null) return 0;
+
+  }
 
   const memoizeCryptoData = useCallback(
     () => {
       var dps = [];
       currentData.forEach(item => {
-        let date = unixToDate(item[0]);
+        let date = unixToDate(Math.floor(item[0]/1000));
+        let dataDate = `${monthsOfTheYear[date.getMonth()]} ${date.getDate()} ${date.getHours()}:${date.getMinutes() }`;
         let price = item[1].toFixed(2);
-        dps.push({ x: date, y: Number(price) });
+        dps.push({ x: date, y: Number(price), label: dataDate });
       });
-      console.log("Datapoints", dps)
+
       return dps;
     },
     [currentData],
   );
 
-  console.log('Setting up options')
-
   const options = {
     theme: "light2", // "light1", "dark1", "dark2"
     animationEnabled: true,
     zoomEnabled: false,
+    rangeChanging: e =>{
+      AxisX viewportMinimum :
+    },
     title: {
       text: `${cryptoName}`
     },
@@ -168,11 +154,9 @@ export const Graph = (props) => {
   }
 
   if (currentData.length === 0) {
-    // console.log(currentData, "in the null condi")
     return null;
   }
   else {
-    // console.log(currentData, "in the not null condi")
     return (
       <div>
         <CanvasJSChart options={options}

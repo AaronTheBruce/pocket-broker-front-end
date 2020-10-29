39c25fc6c729105cd85a18dd543a2f81e5aed954
Aaron Bruce <aaronbruce555@gmail.com>
2020-10-29 14:47:20 -0400

    SavePoint: Incomplete Dynamic render of cryptos from API

--- src/components/protectedRoutes/Cryptos.js
+++ src/components/protectedRoutes/Cryptos.js
@@ -4,20 +4,14 @@ import { ListItem, ListItemIcon, ListItemText, Avatar } from "@material-ui/core"
 // This component should help dynamically render crypto currencies in the WatchList
 //
 const Cryptos = (props) => {
-
   const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
-    props.cryptoHandler(event.currentTarget.id);
-  };
-
   return (
     <ListItem
       button
       id={props.id}
+      key={props.id}
       selected={selectedIndex === props.i}
-      onClick={(event) => handleListItemClick(event, props.i)}
+      onClick={(event) => props.handleListItemClick(event, props.i)}
     >
       {/* <ListItemIcon>
         <Avatar alt="bitcoin-logo" src="/images/bitcoin-logo.png" />
